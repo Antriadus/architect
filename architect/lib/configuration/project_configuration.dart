1dbd6c905b026b7c9e121e6ef41d713dc70bcdde
Kamil Mrowiec <kamil.mrowiec@iteo.com>
2021-08-01 15:16:59 +0200

    Repaired configuration file parsing

--- architect/lib/configuration/project_configuration.dart
+++ architect/lib/configuration/project_configuration.dart
@@ -17,18 +17,26 @@ class ProjectConfiguration {
   );
 
   factory ProjectConfiguration.fromMap(Map<dynamic, dynamic> map) {
-    final layers = List<Layer>.from(
-      map['layers']?.map((x) => Layer.fromMap(x)),
-    );
-    final excludePaths = List<RegExp>.from(
-      (map['excludePaths']?.map((x) => Regex.fromMap(x)) ?? []),
-    );
-    final bannedImportsList = List<BannedImports>.from(
-      map['bannedImports']?.map((x) => BannedImports.fromMap(x)),
-    );
-    final bannedClassNamesList = List<BannedClassName>.from(
-      map['bannedClassNames']?.map((x) => BannedClassName.fromMap(x)),
-    );
+    final layers = map['layers'] == null
+        ? <Layer>[]
+        : List<Layer>.from(
+            (map['layers'] as Iterable).map((x) => Layer.fromMap(x)),
+          );
+    final excludePaths = map['excludePaths'] == null
+        ? <RegExp>[]
+        : List<RegExp>.from(
+            ((map['excludePaths'] as Iterable).map((x) => Regex.fromMap(x))),
+          );
+    final bannedImportsList = map['bannedImports'] == null
+        ? <BannedImports>[]
+        : List<BannedImports>.from(
+            (map['bannedImports'] as Iterable).map((x) => BannedImports.fromMap(x)),
+          );
+    final bannedClassNamesList = map['bannedClassNames'] == null
+        ? <BannedClassName>[]
+        : List<BannedClassName>.from(
+            (map['bannedClassNames'] as Iterable).map((x) => BannedClassName.fromMap(x)),
+          );
 
     final bannedImportsMap = <Layer, Set<Layer>>{};
     for (final bannedConnection in bannedImportsList) {
